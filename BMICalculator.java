d91a72c6dc3c773e0f65d196988cf99982286dd9
Victoria Lin <victoria91718@gmail.com>
2022-10-08 18:45:46 +0800

    feat: add w

--- BMICalculator.java
+++ BMICalculator.java
@@ -12,6 +12,7 @@ public class BMICalculator
       int weight; 
       int sum;
 	  int result = 0;
+	  char index = 'w';
 
       System.out.print("Welcome to the BMI (Body Mass Index) calculator");
       System.out.print("\nPlease enter your weight (kilograms): "); // prompt 
